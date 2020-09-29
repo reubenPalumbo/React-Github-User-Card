2bdc531071e6cb0b9122af5f1353599a48e94ced
Reuben Palumbo <reubensandwichthe1st@gmail.com>
2020-09-29 12:28:18 -0800

    Checkpoint 2

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -5,16 +5,23 @@ import Card from "./Components/Card";
 class App extends React.Component {
   state = {
     profile: {},
-    friends: [],
+    followers: [],
   };
 
   componentDidMount() {
     fetch("https://api.github.com/users/reubenpalumbo")
-      .then((res) => {
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
         this.setState({ profile: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    fetch("https://api.github.com/users/reubenpalumbo/followers")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ followers: data });
       })
       .catch((err) => {
         console.log(err);
@@ -25,7 +32,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Card profile={this.state.profile} />
+          <Card profile={this.state.profile} followers={this.state.followers} />
         </header>
       </div>
     );
